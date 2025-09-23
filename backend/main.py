8b8c3f5434d6384671df8c6ec9182ef4f2f35501
kevinhc2 <92865232+kevinhc2@users.noreply.github.com>
2025-09-22 22:39:53 -0400

    test endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
+from pydantic import BaseModel
 import requests
 import os
 
@@ -11,6 +12,23 @@ if not POLYGON_API_KEY:
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello World"}
+fake_db = {}
+
+# class OptionContract(BaseModel):
+#     ticker: str
+#     contract_type: str # Either call or put
+#     exercise_style: str # Either american or european
+#     strike_price: float
+#     expiration_date: str # In the format YYYY-MM-DD
+
+async def get_options_contract(ticker: str):
+    url = f"https://api.polygon.io/v3/reference/options/contracts/{ticker}?apiKey={POLYGON_API_KEY}"
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise HTTPException(status_code=500, detail="Failed to fetch data from Polygon API")
+    return res.json()
+
+@app.get("/option/{ticker}")
+async def read_option(ticker: str):
+    data = await get_options_contract(ticker)
+    return data
