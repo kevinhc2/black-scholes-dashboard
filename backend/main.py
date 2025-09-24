ee057849e2a67efcdb8a5381b6dda7780aa2a6e4
kevinhc2 <92865232+kevinhc2@users.noreply.github.com>
2025-09-23 22:38:52 -0400

    added class

--- backend/main.py
+++ backend/main.py
@@ -12,23 +12,33 @@ if not POLYGON_API_KEY:
 
 app = FastAPI()
 
-fake_db = {}
+fake_user_option_db = {}
+fake_option_db = {}
 
-# class OptionContract(BaseModel):
-#     ticker: str
-#     contract_type: str # Either call or put
-#     exercise_style: str # Either american or european
-#     strike_price: float
-#     expiration_date: str # In the format YYYY-MM-DD
+class OptionContract(BaseModel):
+    contract_type: str # Either call or put
+    exercise_style: str # Either american or european
+    strike_price: float
+    expiration_date: str # In the format YYYY-MM-DD
 
-async def get_options_contract(ticker: str):
-    url = f"https://api.polygon.io/v3/reference/options/contracts/{ticker}?apiKey={POLYGON_API_KEY}"
-    res = requests.get(url)
-    if res.status_code != 200:
-        raise HTTPException(status_code=500, detail="Failed to fetch data from Polygon API")
-    return res.json()
+def get_options_contract(ticker: str):
+    if ticker not in fake_option_db:
+        url = f"https://api.polygon.io/v3/reference/options/contracts/{ticker}?apiKey={POLYGON_API_KEY}"
+        res = requests.get(url)
+        if res.status_code != 200:
+            raise HTTPException(status_code=500, detail="Failed to fetch data from Polygon API")
+        fake_option_db[ticker] = res.json()
+        return res.json()
+    return fake_option_db[ticker]
 
 @app.get("/option/{ticker}")
-async def read_option(ticker: str):
-    data = await get_options_contract(ticker)
+def read_option(ticker: str):
+    data = get_options_contract(ticker)
     return data
+
+@app.post("/option/create")
+def create_option(option: OptionContract):
+    id = len(fake_user_option_db) + 1
+    option_dict = option.dict()
+    fake_user_option_db[id] = option_dict
+    return {"id": id, **option_dict}
